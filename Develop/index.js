08a6dc67997e741509bf724fc4fbd33fa150bfd9
Hannah Perreault <hperre@gmail.com>
2020-05-31 21:48:19 -0400

    npm init, pseudo coded

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,30 @@
-const questions = [
+//npm init
 
+// The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+
+const questions = [
+  //array of questions
+  // * At least one badge
+  // What is your Project title? [string]
+  // What is your Description? [string]
+  // What is your Table of Contents? [array:string]
+  // How to Install: [string]
+  // What is the Usage? [string]
+  // What is your License? [string]
+  // Who are your Contributors? {array of string}
+  // What are your Tests? [string]
+  // What are your Questions? [array: string]
+  // What is your  GitHub User profile picture? [string: image url?]
+  // What is your GitHub User email? [string]
 ];
 
 function writeToFile(fileName, data) {
+  //where do i want the file placed?
+  //create a file with name fileName
+  //write to fileName the data
+  //09-06
 }
 
-function init() {
-
-}
+function init() {}
 
 init();
